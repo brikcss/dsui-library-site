e9d29e1609cd0a1d8cab914974a2a8f6ca9f8ef5
Zimmee <thezimmee@gmail.com>
2018-04-18 16:54:05 -0600

    fix(supernav): `Close supernav` when route changes.
    [BRANCH: master]

--- src/angularjs/app.controller.js
+++ src/angularjs/app.controller.js
@@ -1,9 +1,19 @@
-function appController(sidebarsService) {
+function appController(sidebarsService, $scope) {
 	var ctrl = this;
 	ctrl.sidebars = sidebarsService;
 	ctrl.showRhythmGrid = false;
+
+	$scope.$on('$locationChangeStart', () => {
+		// Close sidebars when route changes.
+		if (ctrl.sidebars.left.active) {
+			ctrl.sidebars.close('left');
+		}
+		if (ctrl.sidebars.right.active) {
+			ctrl.sidebars.close('right');
+		}
+	});
 }
 
-appController.$inject = ['sidebarsService'];
+appController.$inject = ['sidebarsService', '$scope'];
 
 export default appController;
